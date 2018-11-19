f90cec1c1911f8aa12f76e471441913396bb0b66
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2018-11-20 00:59:12 +0300

    Add files via upload

---
+++ Source code/BookElements/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RecipeBook.Gui;
+
+namespace RecipeBook.BookElements
+{
+    public enum Category : long
+    {
+        [GuiInfo(DisplayText = "Pasta and Rice")]
+        PastaAndRice,
+        Vegetables,
+        Cereal,
+        Fruits,
+        Baking,
+        Snacks,
+        Beverages,
+        Produce,
+        Dairy,
+        [GuiInfo(DisplayText = "Sauces, Spices, and Condiments")]
+        SaucesSpicesAndCondiments,        
+        Canned,
+        [GuiInfo(DisplayText = "Milk products")]
+        MilkProducts,
+        [GuiInfo(DisplayText = "Bread,Other Breakfast Items")]
+        BreadAndOtherBreakfast,
+        Meat,
+        Frozen,
+        Other
+    }
+}
