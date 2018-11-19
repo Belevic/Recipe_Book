a83b71eb57ee8ad4692a1be9e7f0495038e58744
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2018-11-19 18:55:46 +0300

    Add files via upload

---
+++ Source code/BookElements/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+using System.Xml.Serialization;
+
+namespace RecipeBook.Domain
+{
+    public class Ingredient : BookObject
+    {
+        public Category Category { get; set; }
+        public Guid ItemId { get; set; }
+    }
+}
