f96dd7ff93c00a4fb02ef1bf310d186ceb4e1264
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2018-11-19 19:39:13 +0300

    Add files via upload

---
+++ Source code/BookElements/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+using System.Xml.Serialization;
+using RecipeBook.Gui;
+
+namespace RecipeBook.BookElements
+{
+    [GuiInfo(DisplayText = "recipe")]
+    public class Recipe : BookObject
+    {
+        public List<Ingredient> Ingredients { get; set; }
+        public string Steps { get; set; }
+        public string Notes { get; set; }
+        public string Source { get; set; }
+    }
+}
