f584875feb9f944f27cf2c74cc1b4271fc4788bf
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2018-11-20 01:10:14 +0300

    Add files via upload

---
+++ Source code/BookElements/IRecipeBook.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecipeBook.BookElements
+{
+    public interface IRecipeBook
+    {
+        IEnumerable<Recipe> RecipesList { get; }
+        IEnumerable<Ingredient> IngredientsList { get; }
+
+        string SavePath { get; set; }
+        
+        T Get<T>(Guid id) where T : BookObject;
+        IEnumerable<T> GetAll<T>() where T : BookObject;
+        void Add<T>(T domainObject) where T : BookObject;
+        void Remove(Guid id);        
+        void Save();
+    }
+
+    public static class RecipeBookAction
+    {
+        public static void Remove(this IRecipeBook recipeBook, BookObject domainObject)
+        {
+            recipeBook.Remove(domainObject.Id);
+        }
+    }
+}
