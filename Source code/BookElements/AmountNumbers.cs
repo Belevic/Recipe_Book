cbbc3c43697312495202622ea73de5757006750d
Belevich Ilya <37711653+Belevic@users.noreply.github.com>
2018-11-20 01:05:49 +0300

    Add files via upload

---
+++ Source code/BookElements/AmountNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecipeBook.BookElements
+{
+    public static class AmountNumbers
+    {
+        private static readonly IEnumerable<double> defaultNumbers= new[] { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0,7.0,8.0,9.0,10.0 };
+        public static IEnumerable<double> DefaultNumbers
+        {
+            get { return defaultNumbers; }
+        }
+    }
+}
